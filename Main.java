31412e083a583eb7bb0bb2d8fac5ced03e050161
Mirage-code-silly <halolover2343@gmail.com>
2026-03-09 08:03:56 -0500

    Added a print statment

--- Main.java
+++ Main.java
@@ -1,7 +1,8 @@
 public class Main
 {
-public static void Main(String[] args){
-  System.out.println("hello world"_
-}
+  public static void Main(String[] args){
+  System.out.println("hello world");
+  System.out.println("test");
+  }
 
 }
